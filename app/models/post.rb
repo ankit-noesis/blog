ac6b78c5215ac03c3321dfa7472b5da0d965278c
ankit <ankit@noesis.tech>
2019-04-11 11:42:35 +0530

    - Generate Post and Category models. - Create associations between User, Post and Category models. - Create categories and posts database tables.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+	belongs_to :user
+  	belongs_to :category
+  	
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
                     length: { minimum: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_045232) do
+ActiveRecord::Schema.define(version: 2019_04_11_060906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "branch"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -27,8 +34,12 @@ ActiveRecord::Schema.define(version: 2019_04_11_045232) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.bigint "category_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
